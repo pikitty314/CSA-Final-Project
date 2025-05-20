fa38079d2e76ebe943ee75bdfa3316f1da84edf0
pikitty314 <320948@aaps.k12.mi.us>
2025-05-20 13:48:07 -0400

    Start work on new pathfinder

---
+++ TDGame/BetterPathfinder.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class BetterPathfinder
+{
+    static int count = 0;
+    public static boolean legalPoint(boolean[][] grid, Point point)
+    {
+        int x = point.getX();
+        int y = point.getY();
+        
+        if (x < 0 ||
+            y < 0 || 
+            x >= grid[0].length || 
+            y >= grid.length || 
+            !grid[y][x])
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    public static ArrayList<Point> findNeighbors(boolean[][] grid, Point point)
+    {
+        ArrayList<Point> neighbors = new ArrayList<Point>();
+        int x = point.getX();
+        int y = point.getY();
+        
+        Point up = new Point(x, y + 1);
+        Point down = new Point(x, y - 1);
+        Point right = new Point(x + 1, y);
+        Point left = new Point(x - 1, y);
+        
+        if (legalPoint(grid, up))
+        {
+            neighbors.add(up);
+        }
+        
+        if (legalPoint(grid, down))
+        {
+            neighbors.add(down);
+        }
+        
+        if (legalPoint(grid, right))
+        {
+            neighbors.add(right);
+        }
+        
+        if (legalPoint(grid, left))
+        {
+            neighbors.add(left);
+        }
+        
+        return neighbors;
+    }
+    
+    
+    /**
+     * A hopefully better pathfinder.
+     * 
+     * @param grid A 2D array containing a boolean to represent each point that you are or are not allowed to go.
+     * @param start The point to start at.
+     * @param end The point to end at.
+     * @return The path, in order with the first step first and the last step last in the List.
+     */
+    public static ArrayList<Point> pathfinder(boolean[][] grid, Point start, Point end)
+    {    
+        boolean pathFound = false;
+        
+        return null;
+    }
+}
